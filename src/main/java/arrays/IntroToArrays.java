59e954a23d14d2d7634fb04603a0e21180fe3f38
Mv3r4s <127808367+Mv3r4s@users.noreply.github.com>
2023-03-19 00:45:58 -0500

    kk

---
+++ src/main/java/arrays/IntroToArrays.java
@@ -0,0 +1,57 @@
+package arrays;
+
+public class IntroToArrays {
+/*
+4 string arrays
+4 int arrays
+4 double arrays
+4 char arrays
+4 boolean arrays
+ */
+public static void main(String[] args) {
+    // 4 String arrays
+    String[] fruits = {"apple", "banana", "orange", "kiwi"};
+
+
+    String[] cars = {"toyota", "honda", "ford", "chevy"};
+    String[] cities = {"new york", "los angeles", "chicago", "houston"};
+    String[] animals = {"dog", "cat", "bird", "fish"};
+    String [] cars1 = new String[4];
+
+    // 4 int arrays
+    int[] ages = {20, 30, 40, 50};
+    int[] scores = {90, 85, 95, 80};
+    int[] heights = {170, 175, 180, 185};
+    int[] weights = {60, 70, 80, 90};
+
+    // 4 double arrays
+    double[] prices = {1.99, 2.99, 3.99, 4.99};
+    double[] rates = {0.01, 0.02, 0.03, 0.04};
+    double[] temperatures = {20.5, 25.5, 30.5, 35.5};
+    double[] weightsInKg = {65.4, 75.2, 85.0, 95.1};
+
+    // 4 char arrays
+    char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+    char[] consonants = {'b', 'c', 'd', 'f', 'g'};
+    char[] digits = {'0', '1', '2', '3', '4'};
+    char[] letters = {'a', 'b', 'c', 'd', 'e'};
+
+    // 4 boolean arrays
+    boolean[] isAdult = {true, false, true, true};
+    boolean[] isMale = {true, false, true, false};
+    boolean[] isTall = {false, true, false, true};
+    boolean[] isEmployed = {true, false, true, false};
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
